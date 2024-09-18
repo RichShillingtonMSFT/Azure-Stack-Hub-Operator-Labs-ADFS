86999fee61aa99dc1c68560e66e56513a236b349
Rich Shillington <rich@shillington.tv>
2024-09-18 12:06:25 -0400

    Commit

---
+++ Service Principals/Create-ServicePrincipalwithCertAuth.ps1
@@ -0,0 +1,37 @@
+﻿$PEPIP = ''
+$AppName = ''
+$TenantID = ''
+
+# Connect to PEP Session
+$Password = 'PEPPassword' | ConvertTo-SecureString -asPlainText -Force
+$PEPCredentials = New-Object System.Management.Automation.PSCredential('AzureStack\CloudAdmin',$Password)
+
+$PEPSession = New-PSSession -ComputerName $PEPIP -ConfigurationName PrivilegedEndpoint -Credential $PEPCredentials -SessionOption (New-PSSessionOption -Culture en-US -UICulture en-US)
+
+# Create New Self signed cert
+$NewCert = New-SelfSignedCertificate -CertStoreLocation "cert:\CurrentUser\My" -Subject "CN=$AppName" -KeySpec KeyExchange
+
+$Cert = Get-Item "Cert:\CurrentUser\My\$($NewCert.Thumbprint)"
+
+# Create Service Principal
+$SpObject = Invoke-Command -Session $PEPSession -ScriptBlock {New-GraphApplication -Name "$AppName" -ClientCertificates $using:cert}
+
+# Create Service Principal Credential Object
+
+# Sign in using the new service principal
+Connect-AzAccount -Environment "Azs-User" -ServicePrincipal -CertificateThumbprint $SpObject.Thumbprint -ApplicationId $SpObject.ClientId -TenantId $TenantID
+
+Connect-AzAccount -Environment "Azs-User" -ServicePrincipal -CertificateThumbprint $SpObject.Thumbprint -ApplicationId $SpObject.ClientId -TenantId $TenantID
+
+# Use the privileged endpoint to update the client secret, used by <AppIdentifier>
+$SpObject = Invoke-Command -Session $PEPSession -ScriptBlock {Set-GraphApplication -ApplicationIdentifier "<AppIdentifier>" -ClientCertificates $using:NewCert}
+
+# Output the updated service principal details
+$SpObject
+
+
+# Use the privileged endpoint to get a list of applications registered in AD FS
+$AppList = Invoke-Command -Session $PEPSession -ScriptBlock {Get-GraphApplication}
+
+# Use the privileged endpoint to remove application <AppIdentifier>
+Invoke-Command -Session $PEPSession -ScriptBlock {Remove-GraphApplication -ApplicationIdentifier "<AppIdentifier>"}
--- Service Principals/Create-ServicePrincipalwithClientSecret.ps1
+++ Service Principals/Create-ServicePrincipalwithClientSecret.ps1
@@ -1,11 +1,15 @@
-﻿# Connect to PEP Session
+﻿$PEPIP = ''
+$AppName = ''
+$TenantID = ''
+
+# Connect to PEP Session
 $Password = 'PEPPassword' | ConvertTo-SecureString -asPlainText -Force
 $PEPCredentials = New-Object System.Management.Automation.PSCredential('AzureStack\CloudAdmin',$Password)
 
-$PEPSession = New-PSSession -ComputerName 'PutPEPIPHere' -ConfigurationName PrivilegedEndpoint -Credential $PEPCredentials -SessionOption (New-PSSessionOption -Culture en-US -UICulture en-US)
+$PEPSession = New-PSSession -ComputerName $PEPIP -ConfigurationName PrivilegedEndpoint -Credential $PEPCredentials -SessionOption (New-PSSessionOption -Culture en-US -UICulture en-US)
 
 # Create Service Principal
-$SpObject = Invoke-Command -Session $PEPSession -ScriptBlock {New-GraphApplication -Name "APPName" -GenerateClientSecret}
+$SpObject = Invoke-Command -Session $PEPSession -ScriptBlock {New-GraphApplication -Name $AppName -GenerateClientSecret}
 
 # Create Service Principal Credential Object
 $SecurePassword = $SpObject.ClientSecret | ConvertTo-SecureString -AsPlainText -Force
@@ -13,9 +17,9 @@ $SecurePassword = $SpObject.ClientSecret | ConvertTo-SecureString -AsPlainText -
 $SPCredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $SpObject.ClientId, $SecurePassword
 
 # Sign in using the new service principal
-Connect-AzAccount -Environment "AzS-Admin" -ServicePrincipal -Credential $SPCredential -Tenant 'PutTenantIdHere'
+Connect-AzAccount -Environment "AzS-Admin" -ServicePrincipal -Credential $SPCredential -Tenant $TenantID
 
-Connect-AzAccount -Environment "AzS-User" -ServicePrincipal -Credential $SPCredential -Tenant 'PutTenantIdHere'
+Connect-AzAccount -Environment "AzS-User" -ServicePrincipal -Credential $SPCredential -Tenant $TenantID
 
 
 # Use the privileged endpoint to update the client secret, used by <AppIdentifier>
